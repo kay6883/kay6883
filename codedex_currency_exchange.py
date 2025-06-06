bd15dda13920c9cc6ea5f25fd0d7c3ec1c611e12
Kay <143858171+kay6883@users.noreply.github.com>
2025-06-06 02:09:02 -0700

    Create codedex_currency_exchange.py
    codedex challenge to create a currency exchange that will allow user to input currency amount, change foreign currency to american dollars then add it together.
    
    Signed-off-by: Kay <143858171+kay6883@users.noreply.github.com>

---
+++ codedex_currency_exchange.py
@@ -0,0 +1,12 @@
+pesos = int (input("What do you have left in pesos?"));
+#1 peso = .05
+soles = int (input("What do you have left in soles?"));
+#1 soles = .27
+reais = int (input("What do you have left in reais?"));
+#1 reais = .17
+
+pesos = pesos * .05
+soles = soles * .27
+reais = reais * .17
+
+print(pesos + soles + reais)
